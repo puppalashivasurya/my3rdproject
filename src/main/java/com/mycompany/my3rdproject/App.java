c8472d04b2ee6ef568c57a4bc9cc59066f5d2b26
puppalashivasurya <153800602+puppalashivasurya@users.noreply.github.com>
2025-12-16 18:43:40 +0530

    Delete src/main/java/com/mycompany/my3rdproject directory

--- src/main/java/com/mycompany/my3rdproject/App.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.my3rdproject;
-
-public class App {
-    public static void main(String[] args) {
-        int x = 10 / 0;   // BUG for SonarQube
-        System.out.println(x);
-    }
-}

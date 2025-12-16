8a7df2c15a6df733df8401e4a92f80ae20e00284
puppalashivasurya <153800602+puppalashivasurya@users.noreply.github.com>
2025-12-16 11:42:56 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.printMessage();
+    }
+
+    public void printMessage() {
+        System.out.println("Welcome to my3rdproject CI pipeline");
+    }
+}
